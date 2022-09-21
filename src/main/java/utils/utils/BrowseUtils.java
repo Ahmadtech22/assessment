c9fc81944498e39a7e92dea4f08a9511db1db7af
ahmadTech22 <amansour3991@gmail.com>
2022-09-21 06:25:09 -0500

    framework

--- src/main/java/utils/utils/BrowseUtils.java
+++ src/main/java/utils/utils/BrowseUtils.java
@@ -16,96 +16,12 @@ public class BrowseUtils {
     //            3 - less amount of time
 
 
-    public static void selectBy(WebElement element, String value, String methodNAme) {
-
-        Select select = new Select(element);
-
-        switch (methodNAme) {
-
-            case "text":
-                select.selectByVisibleText(value);
-                break;
-            case "value":
-                select.selectByValue(value);
-                break;
-            case "index":
-                select.selectByIndex(Integer.parseInt(value));
-                break;
-            default:
-                System.out.println("please provide text or value or index for method name ");
-                break;
-
-        }
-
-
-    }
-
     public static String getText(WebElement element) {
 
         return element.getText().trim();
     }
 
-    public static String getTitleWithJS(WebDriver driver) {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
 
-        String title = js.executeScript("return document.title").toString();
-
-        return title;
-    }
-
-    public static void clickWithJS(WebDriver driver, WebElement element) {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].click()", element);
-
-
-    }
-
-    public static void scrollwithJS(WebDriver driver, WebElement element) {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView(true)", element);
-
-
-    }
-
-
-    public static void switchByID(WebDriver driver, String mainID) {
-
-        Set<String> allPageID = driver.getWindowHandles();
-        for (String id : allPageID) {
-
-            if (!id.equals(mainID)) {
-                driver.switchTo().window(id);
-
-            }
-        }
-    }
-
-
-    public static void switchByTitle(WebDriver driver, String title) {
-        Set<String> allIDs = driver.getWindowHandles();
-        for (String id : allIDs) {
-            driver.switchTo().window(id);
-            if (driver.getTitle().contains(title)) {
-
-                break;
-            }
-
-
-        }
-    }
-
-    public static void getScreenshot(WebDriver driver,String packageName){
-
-        File file=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        String location=System.getProperty("user.dir")+"/src/java/screenshot"+packageName;
-
-        try {
-            FileUtils.copyFile(file,new File(location+System.currentTimeMillis()));
-            //System.currentTimeMillis will make the data unique all time
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
 
 
